ec48ea345be25ecbff4bb2551bac6ac8dbdc5bda
kamran raza <kr255@njit.edu>
2021-06-28 18:42:22 -0400

    created test for subtraction

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -11,7 +11,11 @@ class MyTestCase(unittest.TestCase):
 
     def test_addition(self):
         calc = Calculator()
-        self.assertisEqual(calc.addtion(2+2), )
+        self.assertisEqual(calc.addtion(2+2), 4)
+
+    def test_subtraction(self):
+        calc = Calculator()
+        self.assertisEqual(calc.addtion(2-2), 0)
 
 if __name__ == '__main__':
     unittest.main()
