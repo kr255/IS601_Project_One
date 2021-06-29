bdef3f008de1ba620569f19db2d15353c6b6be97
kamran raza <kr255@njit.edu>
2021-06-28 20:52:38 -0400

    created divide and multiply in Calculator

--- src/Calculator.py
+++ src/Calculator.py
@@ -4,6 +4,11 @@ def addition(arg1, arg2):
 def subtract(arg1,arg2):
     return arg1-arg2
 
+def divide(arg1, arg2):
+    return arg1/arg2
+
+def multiply(arg1, arg2):
+    return arg1*arg2
 
 class Calculator:
     def __init__(self):
