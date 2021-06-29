faa9b90b3f013953171178b9941ff58fcd363e3e
kamran raza <kr255@njit.edu>
2021-06-28 20:59:05 -0400

    copyPASTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,7 +1,14 @@
-
+from BasicArithmitic import *
 class Calculator:
     def __init__(self):
+
         pass
 
     def add(self, a, b):
         return addition(a, b)
+    def sub(self, a, b):
+        return subtract(a, b)
+    def div(self, a, b):
+        return divide(a, b)
+    def mul(self, a, b):
+        return multiply(a, b)
--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -1,6 +1,6 @@
 import unittest
 from Calculator import Calculator
-
+from BasicArithmitic import *
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
@@ -9,13 +9,13 @@ class MyTestCase(unittest.TestCase):
         calc = Calculator()
         self.assertIsInstance(calc, Calculator)
 
-    # def test_addition(self):
-    #     calc = Calculator()
-    #     self.assertisEqual(calc.addtion(2+2), 4)
-    #
-    # def test_subtraction(self):
-    #     calc = Calculator()
-    #     self.assertisEqual(calc.addtion(2-2), 0)
+    def test_addition(self):
+        calc = Calculator()
+        self.assertisEqual(calc.addtion(2+2), 4)
+
+    def test_subtraction(self):
+        calc = Calculator()
+        self.assertisEqual(calc.sub(2-2), 0)
 
 if __name__ == '__main__':
     unittest.main()
