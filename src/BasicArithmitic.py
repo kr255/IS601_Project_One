9b073ce038e7c5646bc39286267837de5d2de423
kamran raza <kr255@njit.edu>
2021-06-28 20:55:58 -0400

    moved basic functions to BasicArithmitic.py

---
+++ src/BasicArithmitic.py
@@ -0,0 +1,11 @@
+def addition(arg1, arg2):
+    return arg1+arg2
+
+def subtract(arg1,arg2):
+    return arg1-arg2
+
+def divide(arg1, arg2):
+    return arg1/arg2
+
+def multiply(arg1, arg2):
+    return arg1*arg2
--- src/Calculator.py
+++ src/Calculator.py
@@ -1,18 +1,7 @@
-def addition(arg1, arg2):
-    return arg1+arg2
-
-def subtract(arg1,arg2):
-    return arg1-arg2
-
-def divide(arg1, arg2):
-    return arg1/arg2
-
-def multiply(arg1, arg2):
-    return arg1*arg2
 
 class Calculator:
     def __init__(self):
         pass
 
     def add(self, a, b):
-        ad
+        return addition(a, b)
