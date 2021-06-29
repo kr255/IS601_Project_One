ad66de008891abb29fc6a4b1b46d1883d0fecf0a
kamran raza <kr255@njit.edu>
2021-06-28 21:37:54 -0400

    added the setUp for testing CSV reader

--- src/CSVTest.py
+++ src/CSVTest.py
@@ -2,6 +2,10 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.csvReader = CSVReader('Unit Test Addition')
+
     def test_something(self):
         self.assertEqual(True, True)
 
